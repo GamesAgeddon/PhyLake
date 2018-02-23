4d0dedc33d69479123fc182f8ca43973ba0c8fb6
GamesAgeddon <gamesageddon@gmail.com>
2018-02-23 13:02:45 +0100

    Configured the ripple globally.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
+const globalRippleConfig: RippleGlobalOptions = {
+  animation: {
+    enterDuration: 300,
+    exitDuration: 0
+  }
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig} 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
