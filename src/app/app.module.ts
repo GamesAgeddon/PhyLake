8325692f1f26fd09cca1ecbb5c8bb6d00290d00e
GamesAgeddon <gamesageddon@gmail.com>
2018-03-03 08:43:26 +0100

    Add ServiceWorker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 const GlobalRippleConfig: RippleGlobalOptions = {
