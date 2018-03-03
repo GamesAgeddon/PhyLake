b66763aa0f6f8381f0c4981ff350d35cb6b9f254
GamesAgeddon <gamesageddon@gmail.com>
2018-03-03 14:10:47 +0100

    Revert "Add ServiceWorker"
    This reverts commit 8325692f1f26fd09cca1ecbb5c8bb6d00290d00e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material';
-import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 const GlobalRippleConfig: RippleGlobalOptions = {
